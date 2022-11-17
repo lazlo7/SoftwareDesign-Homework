4b9e87574f9ba1dd2d9fc5c4723a01770f7b2daa
Max Smagin <lazlogogalac777@gmail.com>
2022-11-17 15:48:33 +0300

    1: Added comments to source

--- 1-FizzBuzz/src/net/requef/fizzbuzz/Main.java
+++ 1-FizzBuzz/src/net/requef/fizzbuzz/Main.java
@@ -3,15 +3,21 @@ package net.requef.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        // Fizz Buzz
         for (int i = 1; i <= 100; i++) {
+            // Divisible both by 3 and 5.
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("Fizz Buzz");
-            } else if (i % 3 == 0) {
+            }
+            // Divisible by 3, but not 5.
+            else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            }
+            // Divisible by 5, but not 3
+            else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else {
+            }
+            // Not divisible by 3 or 5.
+            else {
                 System.out.println(i);
             }
         }
